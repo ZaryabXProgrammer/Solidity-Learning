03b0c2535b4cd714917adade2ef498b00415d097
ZaryabXProgrammer <zaryabhaider88@gmail.com>
2025-01-20 01:19:38 +0500

    inheritance contracts added

--- contracts/Twitter-Inheritance-Solution.sol
+++ contracts/Twitter-Inheritance-Solution.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.26;
 
-abstract contract Twitter is Ownable {
+contract Twitter is Ownable {
 
     uint16 public MAX_TWEET_LENGTH = 280;
 
@@ -18,73 +18,55 @@ abstract contract Twitter is Ownable {
 
     mapping(address => Tweet[]) public tweets;
 
- 
-
-
     event TweetCreated(uint256 id, address author, string content, uint256 timestamps);
-
     event TweetLiked(address liker, address tweetAuthor, uint256 id, uint256 newLikeCount);
-
     event TweetUnliked(address unliker, address tweetAuthor, uint256 id, uint256 newLikeCount);
 
-    
+    // Constructor to pass the initial owner to the Ownable contract
+    constructor() Ownable(msg.sender) {
+        // You can add any additional initialization logic here if needed
+        
+    }
 
-   
-
-    function changeTweetLength(uint16 newTweetLength ) public onlyOwner {
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
         MAX_TWEET_LENGTH = newTweetLength;
     }
 
-   
     function createTweet(string memory _tweet) public {
-
-        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long bro: " );
-
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long bro: ");
         
         Tweet memory newTweet = Tweet({
             id: tweets[msg.sender].length,
-            author: msg.sender,      
-            content: _tweet,           
-            timestamps: block.timestamp, 
-            likes: 0                   
+            author: msg.sender,
+            content: _tweet,
+            timestamps: block.timestamp,
+            likes: 0
         });
 
-        // Add the new tweet to the list of tweets for the sender's address in the 'tweets' mapping.
         tweets[msg.sender].push(newTweet);
-
-        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content,
-         newTweet.timestamps);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamps);
     }
 
     function likeTweet(address author, uint256 id) external {
-
-        require(tweets[author][id].id == id, "Tweet Does Not Exist");
-
+        require(id < tweets[author].length && tweets[author][id].id == id, "Tweet Does Not Exist");
+        
         tweets[author][id].likes++;
-
-        emit TweetLiked( msg.sender, author, id, tweets[author][id].likes);
+        emit TweetLiked(msg.sender, author, id, tweets[author][id].likes);
     }
 
     function unlikeTweet(address author, uint256 id) external {
-
-  
-        require(tweets[author][id].id == id, "Tweet Does Not Exist");
+        require(id < tweets[author].length && tweets[author][id].id == id, "Tweet Does Not Exist");
         require(tweets[author][id].likes > 0, "Tweets has no likes");
+        
         tweets[author][id].likes--;
-
         emit TweetUnliked(msg.sender, author, id, tweets[author][id].likes);
     }
 
-  
-    function getTweet( uint256 _i) public view returns (Tweet memory) {
-
-       
+    function getTweet(uint256 _i) public view returns (Tweet memory) {
         return tweets[msg.sender][_i];
     }
 
-  
     function getAllTweets(address _owner) public view returns (Tweet[] memory) {
-
         return tweets[_owner];
     }
 }
