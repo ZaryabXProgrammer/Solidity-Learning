4daca6d7311187bb398d1078469798c924764c54
ZaryabXProgrammer <zaryabhaider88@gmail.com>
2025-01-19 22:27:27 +0500

    twitter inheritance

---
+++ contracts/Inheritance-Solution.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+
+
+contract MultiplayerGame{
+
+    mapping(address => bool) public players;
+
+    function joinGame() public virtual{
+        players[msg.sender] = true;
+    } 
+
+}
+
+contract Game is MultiplayerGame{
+    string public gameName;
+    uint256 public playerCount;
+
+    constructor(string memory _gameName){
+        gameName = _gameName;
+        playerCount = 0;
+    }
+
+    function startGame() public {
+
+    }
+
+    function joinGame() public override {
+
+        super.joinGame(); //Funtions from parent contract
+
+        playerCount++;
+
+    }
+
+}
